1943fe6369b413a4a372e8286b39f9b4c5409eda
mishab <mishabsanu298@gmail.com>
2026-04-12 19:07:35 +0530

    url added

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ["https://akod-care.vercel.app", "http://localhost:3000"],
+  origin: ["https://health-care-umber-five.vercel.app", "http://localhost:3000"],
   credentials: true
 }));
 
